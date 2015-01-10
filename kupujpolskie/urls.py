d8f5bc1e8fa1cabe89e25f36d92db7c42ef1c2db
Paweł Janeczek <pjanecze@gmail.com>
2015-01-10 12:03:31 +0100

    added v1 suffix to api url

--- kupujpolskie/urls.py
+++ kupujpolskie/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^api/', include('api.urls')),
+    url(r'^api/v1/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
