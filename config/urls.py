a0bf01d3b514a4024e370ebca3b0dd8ecc1559be
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-08-19 19:45:14 +0200

    adding support for static files from /static/

--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
