7f1ba53c15fd44e82eea7da0ddeec19f6a9f6c7d
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-08-20 21:05:39 +0200

    pola-staging.herokuapp.com and www.pola-app.pl are the only supported domains for respectively staging and production

--- config/settings/production.py
+++ config/settings/production.py
@@ -54,7 +54,7 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.pola-app.pl", "pola-staging.herokuapp.com"]
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ("gunicorn", )
