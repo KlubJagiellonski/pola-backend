5c90542a74de3d0ac11ff42612baad30e6387af9
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-08-21 19:58:09 +0200

    bringing back fast collect

--- config/settings/production.py
+++ config/settings/production.py
@@ -90,7 +90,7 @@ AWS_HEADERS = {
 # URL that handles the media served from MEDIA_ROOT, used for managing
 # stored files.
 MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
- 
+
 # Static Assests
 # ------------------------
 
