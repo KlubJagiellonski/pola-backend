10bf51eb36a277e72c5e0c8e3242b1e95db8282a
Dzakus <Dzakus13@gmail.com>
2015-09-16 01:38:39 +0200

    Add most popular products query

--- pola/views.py
+++ pola/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from braces.views import LoginRequiredMixin
+from product.models import Product
 
 
 class FrontPageView(LoginRequiredMixin, TemplateView):
@@ -9,6 +10,7 @@ class FrontPageView(LoginRequiredMixin, TemplateView):
         context = super(FrontPageView, self).get_context_data(**kwargs)
         context['oldest_reports'] = set()
         context['newest_reports'] = set()
-        context['most_popular_products'] = set()
+        context['most_popular_products'] = (Product.objects.with_query_count()
+                                            .order_by('query_count')[:10])
         context['most_popular_companies'] = set()
         return context
