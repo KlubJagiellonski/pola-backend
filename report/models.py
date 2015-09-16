64446e41634e8a04792674a79d9de51d10239504
Dzakus <Dzakus13@gmail.com>
2015-09-16 05:04:07 +0200

    Hide bussines relating to status

--- report/models.py
+++ report/models.py
@@ -6,6 +6,16 @@ from django.utils.translation import ugettext_lazy as _
 from product.models import Product
 
 
+class ReportQuerySet(models.QuerySet):
+    def only_open(self):
+        return (self.filter(resolved_at__isnull=True)
+                    .filter(resolved_by__isnull=True))
+
+    def only_resolved(self):
+        return (self.filter(resolved_at__isnull=False)
+                    .filter(resolved_by__isnull=False))
+
+
 class Report(models.Model):
     product = models.ForeignKey(Product)
     client = models.CharField(max_length=40,
@@ -15,9 +25,22 @@ class Report(models.Model):
     resolved_by = models.ForeignKey(settings.AUTH_USER_MODEL,
                                     null=True, blank=True)
     desciption = models.TextField()
+    objects = ReportQuerySet.as_manager()
+
+    def status(self):
+        if self.resolved_at is not None:
+            return self.OPEN
+
+        if self.resolved_by is not None:
+            return self.OPEN
+
+        return self.RESOLVED
 
     def get_absolute_url(self):
         return reverse('report:detail', args=[self.pk])
+
+    OPEN = 1
+    RESOLVED = 2
 
 
 class Attachment(models.Model):
