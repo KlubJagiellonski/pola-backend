17b54e23e0c8bc37942bc23d2a457786b8591179
Dzakus <Dzakus13@gmail.com>
2015-09-16 05:04:28 +0200

    Report's status filter

--- report/filters.py
+++ report/filters.py
@@ -5,7 +5,25 @@ from crispy_forms.layout import Submit
 from django.utils.translation import ugettext_lazy as _
 
 
+class StatusFilter(django_filters.ChoiceFilter):
+    def __init__(self, *args, **kwargs):
+        super(StatusFilter, self).__init__(*args, **kwargs)
+        self.extra['choices'] = (
+            ('', '---------'),
+            ('open', _('Open')),
+            ('resolved', _('Resolved'))
+        )
+
+    def filter(self, qs, value):
+        if value == 'open':
+            return qs.only_open()
+        if value == 'resolved':
+            return qs.only_resolved()
+        return qs
+
+
 class ReportFilter(django_filters.FilterSet):
+    status = StatusFilter()
 
     @property
     def form(self):
@@ -17,7 +35,12 @@ class ReportFilter(django_filters.FilterSet):
                                                css_class="btn-block"))
         return self._form
 
-    plCapital = django_filters.RangeFilter()
-
     class Meta:
         model = Report
+        fields = [
+            'status',
+            'product',
+            'client',
+            'created_at',
+            'resolved_at',
+            'resolved_by']
