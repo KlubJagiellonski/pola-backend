1c542abd407b4874411c95e23ca3bf35eafad331
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-09-18 09:51:57 +0200

    mailgun config

--- config/settings/production.py
+++ config/settings/production.py
@@ -106,12 +106,12 @@ INSTALLED_APPS = ('collectfast', ) + INSTALLED_APPS
 
 # EMAIL
 # ------------------------------------------------------------------------------
-#DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL', default='pola <noreply@pola.pl>')
-#EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
-#MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')
-#MAILGUN_SERVER_NAME = env('DJANGO_MAILGUN_SERVER_NAME')
-#EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default='[pola] ')
-#SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
+DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
+MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')
+MAILGUN_SERVER_NAME = env('DJANGO_MAILGUN_SERVER_NAME')
+EMAIL_SUBJECT_PREFIX = ''
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 # TEMPLATE CONFIGURATION
 # ------------------------------------------------------------------------------
