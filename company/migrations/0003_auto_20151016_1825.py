ce17291ec05e41e423f4049434f268f49ddaac4c
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-10-16 20:28:24 +0200

    database migrations after validating of company fields

---
+++ company/migrations/0003_auto_20151016_1825.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import company.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('company', '0002_auto_20151015_1833'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='company',
+            name='plNotGlobEnt',
+            field=company.models.IntegerRangeField(blank=True, null=True, verbose_name="Isn't it a global enterprise?", choices=[(0, 0), (100, 100)]),
+        ),
+        migrations.AlterField(
+            model_name='company',
+            name='plRegistered',
+            field=company.models.IntegerRangeField(blank=True, null=True, verbose_name='Registered in Poland?', choices=[(0, 0), (100, 100)]),
+        ),
+        migrations.AlterField(
+            model_name='company',
+            name='plRegistered_notes',
+            field=models.TextField(blank=True, null=True, verbose_name='Notes about registered in Poland', choices=[(0, 0), (100, 100)]),
+        ),
+        migrations.AlterField(
+            model_name='company',
+            name='plRnD',
+            field=company.models.IntegerRangeField(blank=True, null=True, verbose_name='Information about R&D center', choices=[(0, 0), (100, 100)]),
+        ),
+        migrations.AlterField(
+            model_name='company',
+            name='plWorkers',
+            field=company.models.IntegerRangeField(blank=True, null=True, verbose_name='Information about workers', choices=[(0, 0), (100, 100)]),
+        ),
+    ]
