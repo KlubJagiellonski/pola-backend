1fbd878031710305c03bc812503d2ef041c480ac
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-10-16 09:27:29 +0200

    updated curl's to test API

--- utils/create_report.sh
+++ utils/create_report.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-curl http://localhost:8000/api/create_report?device_id=kuba-test -H "Accept: application/json" -H "Content-type: application/json" --data '{
+curl http://localhost:8000/a/create_report?device_id=kuba-test -H "Accept: application/json" -H "Content-type: application/json" --data '{
    "description": "Krzesło małe"
 }'
 
-curl http://localhost:8000/api/update_report?device_id=kuba-test&report_id=8 -H "Accept: application/json" -H "Content-type: application/json" --data '{
+curl http://localhost:8000/a/update_report?device_id=kuba-test"&"report_id=15 -H "Accept: application/json" -H "Content-type: application/json" --data '{
    "description": "Krzesło małe 2"
 }'
 
-curl -v -F file=@LICENSE http://localhost:8000/a/attach_file?report_id=8"&"device_id=kuba-test
+curl -v -F file=@LICENSE http://localhost:8000/a/attach_file?report_id=15"&"device_id=kuba-test
