7834dca2d7895aa752bf624c12927d858d208669
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-10-17 18:52:24 +0200

    translating the start view

--- pola/views.py
+++ pola/views.py
@@ -15,16 +15,16 @@ class FrontPageView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, *args, **kwargs):
         c = super(FrontPageView, self).get_context_data(**kwargs)
         c['oldest_reports'] = (Report.objects.only_open()
-                                     .order_by('-created_at')[:10])
-        c['newest_reports'] = (Report.objects.only_open()
                                      .order_by('created_at')[:10])
+        c['newest_reports'] = (Report.objects.only_open()
+                                     .order_by('-created_at')[:10])
         c['most_popular_products'] = (Product.objects
                                              .with_query_count()
                                              .filter(company__isnull=True)
                                              .order_by('-query_count')[:10])
         c['most_popular_companies'] = (Company.objects
                                               .with_query_count()
-                                              .filter(plCapital__isnull=True)
+                                              .filter(verified=False)
                                               .order_by('-query_count')[:10])
         return c
 
