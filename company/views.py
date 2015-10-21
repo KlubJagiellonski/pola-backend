7705d24d473746bb467c03a0bde9416ed42acff5
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-10-21 09:37:53 +0200

    WIP

--- company/views.py
+++ company/views.py
@@ -4,33 +4,48 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django_filters.views import FilterView
 from braces.views import LoginRequiredMixin
-from . import models
+from company.models import Company
 from .filters import CompanyFilter
 from .forms import CompanyForm
 
 
 class CompanyListView(LoginRequiredMixin, FilterView):
-    model = models.Company
+    model = Company
     filterset_class = CompanyFilter
     paginate_by = 25
-    queryset = models.Company.objects.with_query_count().all()
+    queryset = Company.objects.with_query_count().all()
 
 
 class CompanyCreate(LoginRequiredMixin, CreateView):
-    model = models.Company
+    model = Company
     form_class = CompanyForm
 
 
 class CompanyUpdate(LoginRequiredMixin, UpdateView):
-    model = models.Company
+    model = Company
     form_class = CompanyForm
 
 
 class CompanyDelete(LoginRequiredMixin, DeleteView):
-    model = models.Company
+    model = Company
     success_url = reverse_lazy('company:list')
+
+class FieldToDisplay:
+    def __init__(self, name):
+        self.name = name
 
 
 class CompanyDetailView(LoginRequiredMixin, DetailView):
-    model = models.Company
-    queryset = models.Company.objects.with_query_count().all()
+    model = Company
+    queryset = Company.objects.with_query_count().all()
+
+
+    FIELDS_TO_DISPLAY = (
+        'common_name',
+        'plRegistered'
+    )
+    fields = [FieldToDisplay('kuku2'), FieldToDisplay('kuku3')]
+
+    def __init__(self):
+        for field in CompanyDetailView.FIELDS_TO_DISPLAY:
+            self.fields.append(FieldToDisplay('kuku'))
