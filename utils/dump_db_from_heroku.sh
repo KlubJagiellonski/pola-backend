129c22dbe0138df53aa8aaf8f6bdbac12549140f
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-10-25 12:25:47 +0100

    getting db backup from production instead of staging

--- utils/dump_db_from_heroku.sh
+++ utils/dump_db_from_heroku.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#dumps pola-staging db from heroku
+#dumps pola-app db from heroku
 
-DUMP=`heroku pg:backups --app pola-staging | grep '^[a|b]' | head -1 | cut -f1 -d' '`
-FILENAME=`heroku pg:backups --app pola-staging | grep '^[a|b]' | head -1 | cut -f3,4 -d' '`
+DUMP=`heroku pg:backups --app pola-app | grep '^[a|b]' | head -1 | cut -f1 -d' '`
+FILENAME=`heroku pg:backups --app pola-app | grep '^[a|b]' | head -1 | cut -f3,4 -d' '`
 FILENAME=`echo $FILENAME | sed 's/:/_/g' | sed 's/ /_/g'`
 FILENAME=`echo "pola_db_${FILENAME}.dump"`
 
-curl -o $FILENAME `heroku pg:backups public-url $DUMP --app pola-staging`
+curl -o $FILENAME `heroku pg:backups public-url $DUMP --app pola-app`
