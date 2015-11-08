c2ab30bde2dd9d14f84459cdbea68fe554353111
Dzakus <Dzakus13@gmail.com>
2015-11-08 22:22:44 +0100

    Add landing page

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     url(r'^cms/stats$', StatsPageView.as_view(), name="home-stats"),
     url(r'^cms/lang/$',
         TemplateView.as_view(template_name='pages/lang-cms.html'), name="select_lang"),
+    url(r'^land/$',
+        TemplateView.as_view(template_name='index2.html'), name="select_lang"),
     url(r'^about/$',
         TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
