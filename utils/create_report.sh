fd24cda5672c57a72a6d6cf7c4968b67ffe9b7ce
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2015-11-16 20:43:41 +0100

    updated testing scripts

--- utils/create_report.sh
+++ utils/create_report.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-curl http://localhost:8000/a/create_report?device_id=kuba-test -H "Accept: application/json" -H "Content-type: application/json" --data '{
+curl http://192.168.99.100:8000/a/v2/create_report?device_id=kuba-test -H "Accept: application/json" -H "Content-type: application/json" --data '{
    "description": "Krzesło małe"
 }'
 
 curl http://localhost:8000/a/update_report?device_id=kuba-test"&"report_id=15 -H "Accept: application/json" -H "Content-type: application/json" --data '{
    "description": "Krzesło małe 2"
 }'
+
+curl http://192.168.99.100:8000/a/v2/attach_file?device_id=kuba-test"&"report_id=3002 -H "Accept: application/json" -H "Content-type: application/json" --data '{
+   "mime_type": "image/png",
+   "file_ext":"png"
+}'
+
+curl -v -H "x-amz-acl: public-read" -H "Content-Type: image/png" https://pola-app.s3.amazonaws.com/2aa854ae-8c98-11e5-adb5-0242ac110002.png?AWSAccessKeyId=AKIAJ5PTX4GFT7D7HVNQ"&"Expires=1447788488"&"Signature=kBkTcunllS1Ubxp0l19fRVU33cQ%3D --upload-file aa.txt
 
 curl -v -F file=@LICENSE http://localhost:8000/a/attach_file?report_id=15"&"device_id=kuba-test
