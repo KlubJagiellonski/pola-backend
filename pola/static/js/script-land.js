4d1a69211c95a17428a4bab48d0a93cf731675e4
Dzakus <Dzakus13@gmail.com>
2015-12-11 03:26:16 +0100

    Add FAQ section without content

--- pola/static/js/script-land.js
+++ pola/static/js/script-land.js
@@ -9209,6 +9209,20 @@ return jQuery;
 
 }));
 
+(function ($){
+  $('.accordian-header').click(function(){
+    var accordian = $(this).closest('.accordian');
+    accordian.toggleClass('is-open');
+    var accprdian_content = accordian.find('.accordian-content');
+    var aria_hidden = accprdian_content.attr('aria-hidden');
+    if(accordian.hasClass('is-open')){
+      accprdian_content.attr('aria-hidden', 'false');
+    }else{
+      accprdian_content.attr('aria-hidden', 'true');
+    }
+  });
+}) (jQuery);
+
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
