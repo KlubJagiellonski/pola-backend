c31d0eeb4469bd41bea60431bf793dfb746cc93f
Dzakus <Dzakus13@gmail.com>
2016-01-06 18:36:33 +0100

    Search also by company name

--- brand/views.py
+++ brand/views.py
@@ -1,14 +1,18 @@
 from django.http import Http404
 from django.shortcuts import render_to_response
 from company.models import Company
+from django.db.models import Q
+from django.db.models import Count
 
 
 def search(request):
     keyword = request.GET.get('q')
     if keyword:
         try:
+            where = Q(
+                name__icontains=keyword) | Q(brand__name__icontains=keyword)
             companies = Company.objects.filter(
-                brand__name__icontains=keyword)
+                where).annotate(total=Count('name'))
         except Company.DoesNotExist:
             raise Http404("Poll does not exist")
         return render_to_response(
