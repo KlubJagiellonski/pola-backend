2fae7c462da9dbad4ad6f980acfbef8a3d7dc8e3
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2016-01-20 09:53:39 +0100

    removed crash - test passed

--- pola/management/commands/crash.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-
-class Command(BaseCommand):
-    help = 'Requeries all codes for data from ILiM'
-
-    def handle(self, *args, **options):
-        raise ZeroDivisionError
