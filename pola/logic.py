ace3553d5d99e37a913676d256fdbd5b5b7becfe
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2016-05-11 22:08:48 +0200

    reading the product name from ILiM API fixed

--- pola/logic.py
+++ pola/logic.py
@@ -150,7 +150,7 @@ def create_from_api(code, obj, product=None):
         obj_data = obj.get('Data', {}) or {}
         obj_owner = obj_data.get('Owner', {}) or {}
         obj_owner_name = obj_owner.get('Name', None)
-        obj_product = obj.get('Product', {}) or {}
+        obj_product = obj_data.get('Product', {}) or {}
         obj_product_name = obj_product.get('Name', None)
 
     if obj_owner_name:
