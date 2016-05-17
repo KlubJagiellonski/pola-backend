9cf90b68a47b1e3435c12f8f0c7d7ca8ebee9005
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2016-05-17 10:17:24 +0200

    workaround

--- api/views.py
+++ api/views.py
@@ -29,10 +29,10 @@ def get_by_code_v2(request):
                              was_590=stats['was_590'],
                              was_plScore=stats['was_plScore'])
 
-    if product:
-        product.increment_query_count()
-        if product.company:
-            product.company.increment_query_count()
+#    if product:
+#        product.increment_query_count()
+#        if product.company:
+#            product.company.increment_query_count()
 
     return JsonResponse(result)
 
@@ -127,10 +127,10 @@ def get_by_code(request, code):
                          was_590=code.startswith('590'),
                          was_plScore=result['plScore'] is not None)
 
-    if product:
-        product.increment_query_count()
-        if product.company:
-            product.company.increment_query_count()
+#    if product:
+#        product.increment_query_count()
+#        if product.company:
+#            product.company.increment_query_count()
 
     return JsonResponse(result)
 
