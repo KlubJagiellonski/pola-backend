e39b19019ec5476ddb1e7a76e514e2239eba4363
Jakub Lipinski <Jakub.Lipinski@gmail.com>
2016-07-12 21:08:19 +0200

    database maintenance: deleting empty revisions created because of the bug in the Company.get_or_create (fixed previously)

---
+++ pola/management/commands/delete_empty_revisions.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from company.models import Company
+from reversion.models import Version
+
+
+class Command(BaseCommand):
+    help = 'Deletes empty revisions'
+
+    def handle(self, *args, **options):
+        companies = Company.objects.all()
+        for company in companies:
+            if company.name:
+                print company.name.encode('UTF-8')
+            versions = Version.objects.\
+                filter(object_id_int=company.pk,
+                       content_type_id=16,
+                       revision__comment='Firma utworzona automatycznie na '
+                                         'podstawie API ILiM',
+                       revision__user__isnull=True)\
+                .order_by('revision__date_created')
+            first_record = True
+            for version in versions:
+                if first_record:
+                    first_record=False
+                else:
+                    version.delete()
+                    print '.',
