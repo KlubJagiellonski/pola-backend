5b2bc17c4490f2653b432f57b4512d21263abd99
Jakub Lipinski <jakub.lipinski@gmail.com>
2017-06-06 19:22:32 +0200

    some useful sql queries

--- utils/queries.sql
+++ utils/queries.sql
@@ -39,3 +39,72 @@ official_name like '%&%;%' or
 common_name like '%&%;%' OR
 address like '%&%;%'
 ;
+
+update product_product set query_count = (select count(*) from pola_query where pola_query.product_id=product_product.id);
+
+SELECT "company_company"."id", "company_company"."name", SUM("product_product"."query_count") AS "query_count"
+FROM "company_company" JOIN "product_product" ON ( "company_company"."id" = "product_product"."company_id" )
+WHERE "company_company"."verified" = false
+GROUP BY "company_company"."id", "company_company"."name"
+ORDER BY "query_count" DESC LIMIT 10;
+
+update product_product
+set query_count = count(query_count.id)
+from pola_query
+where pola_query.product_id = product_product.id;
+
+
+select code,query_count,name from product_product where query_count>0 and company_id is null and code like '590%'
+order by query_count desc;
+
+select count(*) from company_company;
+select count(*) from pola_query;
+select count(*) from pola_stats;
+select count(*) from product_product;
+select count(*) from report_attachment;
+select count(*) from report_report;
+select count(*) from reversion_revision;
+select count(*) from reversion_version;
+
+
+select to_char(date_created, 'YYYY-MM'), count(*)
+from reversion_revision
+group by to_char(date_created, 'YYYY-MM')
+order by 1 desc;
+
+
+select to_char(date_created, 'YYYY-MM'), user_id, content_type_id, count(*)
+from reversion_version
+left outer join reversion_revision on reversion_revision.id = reversion_version.revision_id
+group by to_char(reversion_revision.date_created, 'YYYY-MM'), user_id, content_type_id
+order by 1 desc, 2 desc, 3 desc;
+
+select user_id, content_type_id, comment, count(*)
+from reversion_version
+full join reversion_revision on reversion_revision.id = reversion_version.revision_id
+group by user_id, content_type_id, comment
+order by 4 desc limit 20;
+
+
+select comment
+from reversion_version
+join reversion_revision on reversion_revision.id = reversion_version.revision_id
+where content_type_id=16 and object_id_int=711
+order by date_created desc limit 10;
+
+select count(*) from reversion_revision
+where id not in (select revision_id from reversion_version);
+
+
+select name, query_count from company_company where "plCapital">50 and ("plWorkers"=100 or "plRnD"=100)
+order by query_count desc limit 20;
+
+SELECT relname,n_live_tup
+  FROM pg_stat_user_tables where n_Live_tup>250
+  ORDER BY n_live_tup DESC;
+
+select sum(n_live_tup) from pg_stat_user_tables;
+
+select product_id,count(*) from report_report
+ join report_attachment on report_attachment.report_id = report_report.id
+group by product_id order by count(*) desc;
