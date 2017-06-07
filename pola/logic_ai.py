c9c62ce92cde70afb96de4e25287c1ecd893c06c
Jakub Lipinski <jakub.lipinski@gmail.com>
2017-06-07 11:03:28 +0200

    debugging

--- pola/logic_ai.py
+++ pola/logic_ai.py
@@ -1,8 +1,8 @@
 import random
 
 def add_ask_for_pics(result):
-    if random.choice([True, False]):
-        result['ask_for_pics']=True
-        result['ask_for_pics_text']='Text'
+#    if random.choice([True, False]):
+    result['ask_for_pics'] = True
+    result['ask_for_pics_text'] = 'Text'
 
     return result
