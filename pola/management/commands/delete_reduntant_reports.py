fe52dcc71a0d714ea9969a55a037d4195241ec72
Jakub Lipinski <jakub.lipinski@gmail.com>
2017-06-28 20:11:45 +0200

    deleting redundant reports

---
+++ pola/management/commands/delete_reduntant_reports.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand, CommandError
+from product.models import Product
+from report.models import Report
+from django.db import connection
+
+class Command(BaseCommand):
+    help = 'Deletes redundant reports'
+
+    def add_arguments(self, parser):
+        parser.add_argument('last_product_id')
+
+    def handle(self, *args, **options):
+        products = Product.objects.filter(pk__gte=options["last_product_id"])\
+            .order_by('id')
+
+        for product in products:
+
+            print "{} (id:{})".format(product.name.encode('UTF-8') if product.name else '', product.id)
+
+            reports = Report.objects\
+                .filter(product=product, client='krs-bot')\
+                .values('id','description')\
+                .order_by('created_at')
+
+            desc = []
+            for report in reports:
+
+                if report.description in desc:
+                    print report.description + ' - deleted'
+                    report.delete()
+                else:
+                    print report.description
+                    desc.insert(report.description)
+
+            break
