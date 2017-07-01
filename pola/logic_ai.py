f06317de4ef65839c3413e406f96ccb1501cfd18
Jakub Lipinski <jakub.lipinski@gmail.com>
2017-07-01 10:51:36 +0200

    UI-related changes

--- pola/logic_ai.py
+++ pola/logic_ai.py
@@ -4,12 +4,12 @@ import random
 def add_ask_for_pics(result):
     if random.choice([True, False]):
         result['ask_for_pics'] = True
-        result['ask_for_pics_card_text'] = 'Naucz Polę jak wygląda ten produkt!'
+        result['ask_for_pics_preview'] = 'Naucz Polę tego produktu'
+        result['ask_for_pics_title'] = 'Naucz Polę tego produktu'
         result['ask_for_pics_text'] = \
             'Pomóż nauczyć Polę rozpoznawania produktów po wyglądzie. ' \
-            'Nakręć film obracając produkt w dłoni tak jak na ' \
+            'Nakręć film obracając produkt tak jak na ' \
             'przykładzie poniżej. Postaraj się nie zasłaniać produktu.'
-        result['ask_for_pics_button_yes_text'] = 'Chętnię pomogę!'
-        result['ask_for_pics_button_no_text'] = 'Może innym razem...'
+        result['ask_for_pics_button_start'] = 'Nakręć film'
 
     return result
