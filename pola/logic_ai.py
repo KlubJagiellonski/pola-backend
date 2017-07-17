144d1220b593c0396f5d50a4e93135d3b81cb424
Jakub Lipinski <jakub.lipinski@gmail.com>
2017-07-17 20:53:44 +0200

    moving AI-related API responses into sub-list

--- pola/logic_ai.py
+++ pola/logic_ai.py
@@ -3,14 +3,15 @@ import random
 
 def add_ask_for_pics(result):
     if random.choice([True, False]):
-        result['ask_for_pics'] = True
-        result['ask_for_pics_preview'] = 'Naucz Polę tego produktu'
-        result['ask_for_pics_title'] = 'Naucz Polę tego produktu'
-        result['ask_for_pics_text'] = \
+        ai = result['ai'] = {}
+        ai['ask_for_pics'] = True
+        ai['ask_for_pics_preview'] = 'Naucz Polę tego produktu'
+        ai['ask_for_pics_title'] = 'Naucz Polę tego produktu'
+        ai['ask_for_pics_text'] = \
             'Pomóż nauczyć Polę rozpoznawania produktów po wyglądzie. ' \
             'Nakręć film obracając produkt tak jak na ' \
             'przykładzie poniżej. Postaraj się nie zasłaniać produktu.'
-        result['ask_for_pics_button_start'] = 'Nakręć film'
-        result['max_pic_size'] = 800
+        ai['ask_for_pics_button_start'] = 'Nakręć film'
+        ai['max_pic_size'] = 800
 
     return result
