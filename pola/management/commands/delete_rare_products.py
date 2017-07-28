ef4b095e1b3d24b4bb31da7a7f06d64bb0017eab
Jakub Lipinski <jakub.lipinski@gmail.com>
2017-07-28 20:14:33 +0200

    faster version of db cleanup

--- pola/management/commands/delete_rare_products.py
+++ pola/management/commands/delete_rare_products.py
@@ -11,33 +11,32 @@ class Command(BaseCommand):
     help = 'Deletes rarely queried products (probably spam)'
 
     def add_arguments(self, parser):
-        parser.add_argument('last_product_id')
+        parser.add_argument('limit')
 
     def handle(self, *args, **options):
-        i=0;
-        products = Product.objects.filter(pk__gte=options["last_product_id"],
-                                          company__isnull=True,
-                                          name__isnull=True)\
-            .order_by('id')[:100000].iterator()
-        for product in products:
-            if i%100 == 0:
-                print
-                print product.id
-            i+=1
+        with connection.cursor() as c:
+            c.execute(
+                "SELECT id "
+                "FROM product_product "
+                "WHERE company_id IS NULL AND name IS NULL "
+                "AND (select count(*) from report_report where product_id=product_product.id)=0 "
+                "AND (select count(*) from pola_query where product_id=product_product.id) < "
+                "(12*date_part('year',age(created_at))+ date_part('month',age(created_at))) limit %s",
+                [options["limit"]]
+            )
+            while True:
+                row = c.fetchone()
+                if not row:
+                    return
 
-            no_of_reports = Report.objects.filter(product=product).count()
-            no_of_queries = Query.objects.filter(product=product).count()
-
-            now = datetime.now()
-            age_in_months = (now.year-product.created_at.year)*12 + (now.month-product.created_at.month)
-
-            # delete if product queried less than once per month
-            if no_of_reports==0 and no_of_queries < age_in_months:
+                product_id = row[0]
+#                print product_id
                 sys.stdout.write('.')
                 sys.stdout.flush()
+#                return
 
                 versions = Version.objects.\
-                    filter(object_id_int=product.id,
+                    filter(object_id_int=product_id,
                            content_type_id=15)\
                     .values('id','revision_id').iterator()
                 with connection.cursor() as cursor:
@@ -48,7 +47,7 @@ class Command(BaseCommand):
                         cursor.execute(
                             'delete from reversion_revision where id=%s',
                             [version['revision_id']])
-                product.delete()
-            else:
-                sys.stdout.write('o')
-                sys.stdout.flush()
+                    cursor.execute(
+                        'delete from product_product where id=%s',
+                        [product_id]
+                    )
