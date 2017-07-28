fd42446eb991b0fca073ac955fd4ec5f3a5dc786
Jakub Lipinski <jakub.lipinski@gmail.com>
2017-07-28 11:48:54 +0200

    commenting out recalculating of the ai pics count as we have some server-side issues

--- pola/management/commands/recalculate_query_count.py
+++ pola/management/commands/recalculate_query_count.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print 'Recalculating product query count'
         Product.recalculate_query_count()
-        print 'Recalculating product ai pics count'
-        Product.recalculate_ai_pics_count()
+#        print 'Recalculating product ai pics count'
+#        Product.recalculate_ai_pics_count()
         print 'Recalculating company query count'
         Company.recalculate_query_count()
         print 'Finished recalculating query count'
