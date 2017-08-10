bbe34fd5f4c4fb48af1b36819006e679ccb937eb
Jakub Lipinski <jakub.lipinski@gmail.com>
2017-08-10 16:15:26 +0200

    update copy script

--- utils/copy_db_from_production_to_staging.sh
+++ utils/copy_db_from_production_to_staging.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-heroku pg:copy pola-app::HEROKU_POSTGRESQL_PURPLE blooming-gently-4582 -a pola-staging
+heroku pg:copy pola-app::DATABASE_URL DATABASE_URL -a pola-staging
+
+# trim database on staging
+# WARNING: DESTRUCTIVE ACTIONS AHEAD
+# run:
+# heroku run -a pola-staging
+# psql $DATABASE_URL
+# truncate pola_query
+## truncate reversion_revision
+## truncate reversion_version
