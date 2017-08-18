d9ccbcc67fb5e94e45419966560753a54d6414f9
mik-laj <kamil.bregula@siecobywatelska.pl>
2017-08-18 15:07:26 +0200

    Add admin for ai_pics

---
+++ ai_pics/admin.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from .models import AIPics, AIAttachment
+
+
+class AIAttachmentAdminInline(admin.TabularInline):
+    model = AIAttachment
+    list_display = ('id', 'ai_pics', 'file_no', 'attachment')
+    list_filter = ('ai_pics',)
+
+
+@admin.register(AIPics)
+class AIPicsAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'created_at',)
+    fields = [
+        'id',
+        'product',
+        'client',
+        'created_at',
+        (
+            'original_width',
+            'original_height',
+            'width',
+            'height'
+        ),
+        'device_name',
+        'flash_used',
+        'was_portrait',
+        'is_valid'
+    ]
+    list_display = (
+        'id',
+        'product',
+        'client',
+        'created_at',
+        'is_valid',
+    )
+    inlines = [
+        AIAttachmentAdminInline
+    ]
+    list_filter = ('product', 'created_at', 'is_valid')
+    date_hierarchy = 'created_at'
+
+
+@admin.register(AIAttachment)
+class AIAttachmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ai_pics', 'file_no', 'attachment')
+    list_filter = ('ai_pics',)
