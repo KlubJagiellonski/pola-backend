606e23333cf4c3a487ba17bd8bf3d3da741c672f
mik-laj <kamil.bregula@siecobywatelska.pl>
2017-09-04 10:53:22 +0200

    Stylistyczne w reports.views Close #20

--- report/views.py
+++ report/views.py
@@ -9,44 +9,44 @@ from django.views.generic.edit import DeleteView
 from django_filters.views import FilterView
 
 from pola.views import ActionView
-from . import models
+from report.models import Report
 from .filters import ReportFilter
 
 
 class ReportListView(LoginRequiredMixin, FilterView):
-    model = models.Report
+    model = Report
     filterset_class = ReportFilter
     paginate_by = 25
-    queryset = models.Report.objects.all().prefetch_related('attachment_set')
+    queryset = Report.objects.prefetch_related('attachment_set').all()
 
 
 class ReportAdvancedListView(LoginRequiredMixin, FilterView):
-    model = models.Report
+    model = Report
     filterset_class = ReportFilter
     paginate_by = 25
     template_name_suffix = '_filter_adv'
-    queryset = models.Report.objects.all().prefetch_related('attachment_set')
+    queryset = Report.objects.all().prefetch_related('attachment_set')
 
     def post(self, request, *args, **kwargs):
         messages.success(request, "Raporty zostały rozpatrzone")
         ids = request.POST.getlist('report_to_resolve')
-        models.Report.objects.filter(pk__in=ids).resolve(self.request.user)
+        Report.objects.filter(pk__in=ids).resolve(self.request.user)
         return HttpResponseRedirect(request.get_full_path())
 
 
 class ReportDeleteView(LoginRequiredMixin, DeleteView):
-    model = models.Report
+    model = Report
     success_url = reverse_lazy('report:list')
 
 
 class ReportDetailView(LoginRequiredMixin, DetailView):
-    model = models.Report
+    model = Report
 
 
 class ReportResolveView(LoginRequiredMixin, ActionView):
-    model = models.Report
+    model = Report
     template_name_suffix = '_resolve'
-    queryset = models.Report.objects.only_open().all()
+    queryset = Report.objects.only_open().all()
 
     def action(self):
         self.object.resolve(self.request.user)
