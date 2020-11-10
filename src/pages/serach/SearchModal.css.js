6a130f831e9cfb6bdba08be75e18efa97803712a
zyzik1248 <62909958+zyzik1248@users.noreply.github.com>
2020-11-10 22:00:12 +0100

    poprawka responsywnoci (#15)

--- src/pages/serach/SearchModal.css.js
+++ src/pages/serach/SearchModal.css.js
@@ -3,6 +3,9 @@ import styled from "styled-components"
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  font-size: 16px;
+  overflow: auto;
+  max-height: 80vh;
 `
 
 export const BigSection = styled.div`
