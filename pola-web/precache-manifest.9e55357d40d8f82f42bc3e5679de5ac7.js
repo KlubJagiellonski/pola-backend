fe357a44c6dd681cae73b04332b76252ce50bb08
zuzanna dudzinska <zuzanna.dudzinska@mayeryn.com>
2020-11-11 22:12:44 +0100

    fix app

---
+++ pola-web/precache-manifest.9e55357d40d8f82f42bc3e5679de5ac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54a55b2704d0ffb362a3f67603dbccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58d91deeab5cdcade27f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b0fa84d718e446cfc8b",
+    "url": "/static/js/2.83e805c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.83e805c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d91deeab5cdcade27f",
+    "url": "/static/js/main.98add99b.chunk.js"
+  },
+  {
+    "revision": "1a5aa3f76c225a7e0da2",
+    "url": "/static/js/runtime-main.d3ec554c.js"
+  }
+]);
--- pola-web/service-worker.js
+++ pola-web/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56338c07c8a44f9d5e196316cf285319.js"
+  "/precache-manifest.9e55357d40d8f82f42bc3e5679de5ac7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
