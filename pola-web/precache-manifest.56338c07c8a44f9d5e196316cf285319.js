8a32047ee732e8f9d2bb2556648281b140ff44b8
Kamil Breguła <kamil.bregula@polidea.com>
2020-11-19 14:32:43 +0100

    fixup! dodanie autorów

---
+++ pola-web/precache-manifest.56338c07c8a44f9d5e196316cf285319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf9b4d277d7415319b5591285466829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fc0c9a2ad6fdf9f958f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eaa3ba1c42ebab097f6e",
+    "url": "/static/js/2.5ba2315d.chunk.js"
+  },
+  {
+    "revision": "fa44b1e8e997ce7ffe81ca75f82bf57d",
+    "url": "/static/js/2.5ba2315d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc0c9a2ad6fdf9f958f",
+    "url": "/static/js/main.28cfc6a6.chunk.js"
+  },
+  {
+    "revision": "1a5aa3f76c225a7e0da2",
+    "url": "/static/js/runtime-main.d3ec554c.js"
+  }
+]);
