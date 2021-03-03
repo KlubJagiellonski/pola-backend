319125294d60b1d86f48d21cf721f640a15fd41e
Mikołaj Rodkiewicz <5458878+rodkiewicz@users.noreply.github.com>
2021-03-03 14:37:42 +0100

    add "resolve all reports with the same product" button

--- report/urls.py
+++ report/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'advanced/$', views.ReportAdvancedListView.as_view(), name="advanced"),
     url(r'(?P<pk>[-\w]+)/delete$', views.ReportDeleteView.as_view(), name="delete"),
     url(r'(?P<pk>[-\w]+)/resolve/$', views.ReportResolveView.as_view(), name="resolve"),
+    url(r'(?P<pk>[-\w]+)/resolve-all/$', views.ReportResolveAllView.as_view(), name="resolve-all"),
     url(r'(?P<pk>[-\w]+)/$', views.ReportDetailView.as_view(), name="detail"),
     url(r'$', views.ReportListView.as_view(), name="list"),
 ]
