46a63a14c15477a290333dd4eb06dae5f1040841
Mikołaj Rodkiewicz <5458878+rodkiewicz@users.noreply.github.com>
2021-03-18 17:52:11 +0100

    pre-commit fix

--- pola/tests/commands/test_import_brands.py
+++ pola/tests/commands/test_import_brands.py
@@ -2,6 +2,7 @@ from django.core.files.temp import NamedTemporaryFile
 from django.core.management import call_command
 from django.test import TestCase
 
+
 class ImportBrandsTestCase(TestCase):
     def test_run_command(self):
         with NamedTemporaryFile() as tmp_file:
