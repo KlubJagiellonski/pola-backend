fb3c52346824865546e4cab297df8f565e3df035
Kamil Breguła <kamilbregula@apache.org>
2021-04-26 02:38:33 +0200

    Simplify code + add tests

--- pola/management/commands/import_brands.py
+++ pola/management/commands/import_brands.py
@@ -1,6 +1,5 @@
 import argparse
 import csv
-import os
 import re
 
 from django.core.management import BaseCommand
@@ -12,7 +11,6 @@ from product.models import Product
 def update_product(self, brand, ean_code, company, product_name):
     product = Product.objects.filter(code=ean_code).first()
     if product:
-        product = Product.objects.get(code=ean_code)
         product.brand = brand
         product.name = product_name
         product.save()
@@ -22,37 +20,49 @@ def update_product(self, brand, ean_code, company, product_name):
         self.stdout.write(self.style.SUCCESS(f"Successfully created product {product_name}"))
 
 
+def nip_number(value):
+    if len(value) == 10 and value.isdigit():
+        return value
+    raise argparse.ArgumentTypeError(f"Invalid NIP number: '{value}'")
+
+
 class Command(BaseCommand):
     help = 'Import lidl companies from .tsv file'
 
     def add_arguments(self, parser):
         parser.add_argument('tsv_filepath', type=argparse.FileType('r'))
-        parser.add_argument('company_nip')
+        parser.add_argument('company_nip', type=nip_number)
+        parser.add_argument(
+            '--noinput',
+            '--no-input',
+            action='store_false',
+            dest='interactive',
+            help=('Tells Django to NOT prompt the user for input of any kind. '),
+        )
 
     def handle(self, *args, **options):
-        if not options['tsv_filepath'] or not os.path.exists(options['tsv_filepath'].name):
-            raise SystemExit("Invalid path")
-        if not options['company_nip'] or len(options['company_nip']) != 10 and options['company_nip'].isdigit():
-            raise SystemExit("Company nip not valid")
         brand_owner = Company.objects.filter(nip__exact=options['company_nip']).first()
         if not brand_owner:
             self.stdout.write(self.style.ERROR(f'Company with nip {options["company_nip"]} does not exist.'))
             return
-        confirm = input(
-            f'You seleceted company: {brand_owner.official_name} with nip: {brand_owner.nip}. Proceed? (Y/n)'
-        )
-        while True:
-            if confirm not in ('Y', 'n', 'yes', 'no'):
-                confirm = input('Please enter either "yes" or "no": ')
-                continue
-            if confirm in ('Y', 'yes'):
-                break
-            else:
-                return
+
+        if options['interactive']:
+            confirm = input(
+                f'You seleceted company: {brand_owner.official_name} with nip: {brand_owner.nip}. Proceed? (Y/n)'
+            )
+            while True:
+                if confirm not in ('Y', 'n', 'yes', 'no'):
+                    confirm = input('Please enter either "yes" or "no": ')
+                    continue
+                if confirm in ('Y', 'yes'):
+                    break
+                else:
+                    return
 
         with options['tsv_filepath'] as csv_file:
             csv_reader = csv.reader(csv_file, delimiter='\t')
             index_successful = 0
+            line_no = 0
             for line_no, row in enumerate(csv_reader):
                 # skip column names
                 if line_no == 0:
@@ -64,16 +74,14 @@ class Command(BaseCommand):
                 product_name = row[5]
 
                 # check if brand exist in db
-                if Brand.objects.filter(company=brand_owner, common_name=brand_name).count():
-                    brand = Brand.objects.filter(company=brand_owner, common_name=brand_name).first()
-                else:
+                brand = Brand.objects.filter(company=brand_owner, common_name=brand_name).first()
+                if not brand:
                     brand = Brand(company=brand_owner, common_name=brand_name)
                     brand.save()
                     self.stdout.write(self.style.SUCCESS(f"Successfully created brand {brand.common_name} "))
                 # check if company from tsv file exist in db
-                if Company.objects.filter(nip__exact=nip).count():
-                    company = Company.objects.get(nip__exact=nip)
-                else:
+                company = Company.objects.filter(nip__exact=nip).first()
+                if not company:
                     self.stdout.write(
                         self.style.WARNING(
                             f'Company with nip {nip} does not exist. '
@@ -83,11 +91,7 @@ class Command(BaseCommand):
                     company = Company(nip=nip, name=row[7])
                     company.save()
 
-                if len(ean_codes) == 1:
-                    # check if product exist in db
-                    update_product(self, brand, ean_codes[0], company, product_name)
-                    index_successful += 1
-                else:
+                if len(ean_codes) > 1:
                     self.stdout.write(
                         self.style.WARNING(
                             f"Product with name {product_name} has multiple ean codes: "
@@ -95,8 +99,11 @@ class Command(BaseCommand):
                             f"Script will create/update multiple products, one for each code."
                         )
                     )
-                    for code in ean_codes:
-                        # check if product exist in db
-                        update_product(self, brand, code, company, product_name)
-                        index_successful += 1
-            self.stdout.write(self.style.SUCCESS(f'Processed {line_no} products successful.'))
+                for code in ean_codes:
+                    # check if product exist in db
+                    update_product(self, brand, code, company, product_name)
+                    index_successful += 1
+            if line_no == 0:
+                self.stdout.write(self.style.SUCCESS('Empty file. Nothing to do'))
+            else:
+                self.stdout.write(self.style.SUCCESS(f'Processed {line_no} products successful.'))
--- pola/tests/commands/test_import_brands.py
+++ pola/tests/commands/test_import_brands.py
@@ -1,9 +1,59 @@
+import os
+import re
+from io import StringIO
+
 from django.core.files.temp import NamedTemporaryFile
 from django.core.management import call_command
 from django.test import TestCase
 
+from company.factories import CompanyFactory
+from company.models import Brand, Company
+from product.models import Product
+
+EXAMPLE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_import_brands_fixture.tsv")
+
+ANSI_ESCAPE_SEQUENCE_REGEXP = re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]")
+
+
+def strip_ansi_escape_sequence(text: str) -> str:
+    """Remove 7-bit C1 ANSI escape sequences"""
+    return ANSI_ESCAPE_SEQUENCE_REGEXP.sub("", text)
+
 
 class ImportBrandsTestCase(TestCase):
-    def test_run_command(self):
+    def test_missing_company(self):
+        out = StringIO()
         with NamedTemporaryFile() as tmp_file:
-            call_command('import_brands', tmp_file.name)
+            call_command('import_brands', tmp_file.name, "1234566789", interactive=False, stdout=out)
+        self.assertIn("Company with nip 1234566789 does not exist.", strip_ansi_escape_sequence(out.getvalue()))
+
+    def test_empty_file(self):
+        CompanyFactory(nip="1234566789")
+        out = StringIO()
+        with NamedTemporaryFile() as tmp_file:
+            call_command('import_brands', tmp_file.name, "1234566789", interactive=False, stdout=out)
+        self.assertIn("Empty file. Nothing to do", strip_ansi_escape_sequence(out.getvalue()))
+
+    def test_example_file(self):
+        CompanyFactory(nip="1234566789")
+        out = StringIO()
+        call_command('import_brands', EXAMPLE_FILE, "1234566789", interactive=False, stdout=out)
+        self.assertIn(
+            'Successfully created brand Pilos \n'
+            'Company with nip 7010399415 does not exist. Script will create a new company with name: '
+            'Mazowiecka Sp.Mlecz. nip: 7010399415\n'
+            'Successfully created product Jogurt pitny 1,5%, czerwona pomarańcza\n'
+            'Successfully created product Jogurt pitny 1,5%, żurawina\n'
+            'Company with nip 7220002329 does not exist. Script will create a new company with name: '
+            'Mlekovita nip: 7220002329\n'
+            'Product with name Serek twarogowy plastry, z ziołami has multiple ean codes: '
+            '20387754, 20980368. Script will create/update multiple products, one for each code.\n'
+            'Successfully created product Serek twarogowy plastry, z ziołami\n'
+            'Successfully created product Serek twarogowy plastry, z ziołami\n'
+            'Processed 3 products successful.',
+            strip_ansi_escape_sequence(out.getvalue()).strip(),
+        )
+        self.assertTrue(Company.objects.filter(nip="7010399415", name="Mazowiecka Sp.Mlecz.").exists())
+        self.assertTrue(Company.objects.filter(nip="7220002329", name="Mlekovita").exists())
+        self.assertEqual(4, Product.objects.filter(code__in=["20268190", "20268176", "20387754", "20980368"]).count())
+        self.assertTrue(Brand.objects.filter(common_name="Pilos").exists())
