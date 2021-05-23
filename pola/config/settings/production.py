2e0ce1f0e407a8932f3d9caccb29f18000bc1214
Kamil Breguła <kamilbregula@apache.org>
2021-05-23 02:40:21 +0200

    Migrate from django-mailgun to django-anymail

--- pola/config/settings/production.py
+++ pola/config/settings/production.py
@@ -69,9 +69,12 @@ INSTALLED_APPS = ('collectfast',) + INSTALLED_APPS  # noqa: F405
 # EMAIL
 # ------------------------------------------------------------------------------
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL')  # noqa: F405
-EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
-MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')  # noqa: F405
-MAILGUN_SERVER_NAME = env('DJANGO_MAILGUN_SERVER_NAME')  # noqa: F405
+ANYMAIL = {
+    "MAILGUN_API_KEY": env('DJANGO_MAILGUN_API_KEY'),  # noqa: F405
+    "MAILGUN_API_URL": "https://api.eu.mailgun.net/v3",  # noqa: F405
+    "MAILGUN_SENDER_DOMAIN": env('DJANGO_MAILGUN_SERVER_NAME'),  # noqa: F405
+}
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 EMAIL_SUBJECT_PREFIX = ''
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
