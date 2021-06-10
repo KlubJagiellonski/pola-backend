c1f0c495fc21a50f35eb67a771e5a3c3e923fed6
Kamil Breguła <kamilbregula@apache.org>
2021-06-10 04:38:05 +0200

    fixup! Display environment status on Github (#717)

--- scripts/detect_app_name.sh
+++ scripts/detect_app_name.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
-test -v GITHUB_BRANCH
+export
+if [[ -z "${GITHUB_BRANCH}" ]]; then
+  echo "GITHUB_BRANCH variable is unset"
+  exit 1
+fi
 
 if [[ "$(echo "${GITHUB_BRANCH}" | cut -d / -f 3)" == "prod" ]]; then
   echo "pola-app"
 elif [[ "$(echo "${GITHUB_BRANCH}" | cut -d / -f 3)" == "master" ]]; then
   echo "pola-staging"
 else
-  >&2 echo "Unknown app."
+  >&2 echo "Unknown app. Current branch: ${GITHUB_BRANCH}"
   exit 1;
 fi
