4689e1ec56b27eb5b0345920ae19a4d8390baf40
Adam Walkowski <adam.walkowski@inventcore.net>
2021-06-29 09:51:38 +0200

    remove prototype code

--- prototype/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders search button', () => {
-  const { getByText } = render(<App />);
-  const button = getByText(/sprawdź/i);
-  expect(button).toBeInTheDocument();
-});
--- prototype/src/history.js
+++
@@ -1,7 +0,0 @@
-import { createBrowserHistory, createMemoryHistory } from "history";
-
-const isTest = process.env.NODE_ENV === "test";
-
-export const history = isTest
-  ? createMemoryHistory({ initialEntries: ['/'] })
-  : createBrowserHistory();
