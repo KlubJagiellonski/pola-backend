a7fa40d4cb2cc08fffc2b7bc26a6a0205b2b28d1
Kamil Breguła <kamilbregula@apache.org>
2021-10-04 00:18:18 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! Add integration for pola-web

--- pola/tests/test_views_pola_web.py
+++ pola/tests/test_views_pola_web.py
@@ -2,6 +2,7 @@ import random
 import string
 import sys
 
+from django.test import override_settings
 from django.urls import clear_url_caches
 from test_plus.test import TestCase
 
@@ -14,8 +15,9 @@ class TestPolaWebView(TestCase):
         self.bucket_name = f"test-bucket-{random_prefix}"
         self.s3_client = create_s3_client()
         self.s3_client.create_bucket(Bucket=self.bucket_name)
-        with self.settings(ENABLE_POLA_WEB_CUSTOMIZATION=True):
-            self._clear_url_caches()
+        self.customization_settings = override_settings(ENABLE_POLA_WEB_CUSTOMIZATION=True)
+        self.customization_settings.enable()
+        self._clear_url_caches()
 
     def _clear_url_caches(self):
         if 'pola.config.urls' in sys.modules:
@@ -25,8 +27,8 @@ class TestPolaWebView(TestCase):
     def tearDown(self) -> None:
         bucket = create_s3_resource().Bucket(self.bucket_name)
         bucket.objects.all().delete()
-
         self.s3_client.delete_bucket(Bucket=self.bucket_name)
+        self.customization_settings.disable()
         self._clear_url_caches()
 
     def test_should_return_404_for_invalid_cms_view(self):
@@ -57,7 +59,7 @@ class TestPolaWebView(TestCase):
             self.assertEqual(content, response.content.decode())
 
     def test_should_return_200_when_index_exists(self):
-        content = "test-index-content.html"
+        content = "index.html"
         self.s3_client.put_object(
             Body=content,
             Bucket=self.bucket_name,
@@ -70,14 +72,14 @@ class TestPolaWebView(TestCase):
             self.assertEqual(content, response.content.decode())
 
     def test_should_return_200_when_file_exists(self):
-        content = "test-content.js"
+        content = "test.js"
         self.s3_client.put_object(
             Body=content,
             Bucket=self.bucket_name,
-            Key="app.js",
+            Key="test.js",
         )
 
         with self.settings(AWS_STORAGE_WEB_BUCKET_NAME=self.bucket_name):
-            response = self.client.get('/app.js')
+            response = self.client.get('/test.js')
             self.assertEqual(response.status_code, 200)
             self.assertEqual(content, response.content.decode())
