51e56412d98ddc9425f54bd1f740e4d3fe400f76
Kamil Breguła <kamilbregula@apache.org>
2021-10-04 02:19:25 +0200

    fixup! fixup! fixup! Add integration for pola-web (#1096)

--- pola/views_pola_web.py
+++ pola/views_pola_web.py
@@ -3,14 +3,20 @@ import os
 from botocore.exceptions import ClientError
 from django.conf import settings
 from django.http import HttpResponse
+from django.utils.cache import add_never_cache_headers
 from django.views import View, defaults
 from django.views.decorators.cache import cache_page
 
 from pola.s3 import create_s3_client
 
+# 256 KB
+MAX_CACHE_KEY_SIZE = int(256 * 1024)
+# 15 minutes
+CACHE_TIMEOUT = 60 * 15
+
 
 class PolaWebView(View):
-    def get_s3_stream_response(self, s3_client, key, status_code=200):
+    def get_s3_data(self, s3_client, key):
         try:
             print(f"PolaWebView:get_s3_stream_response::key={key}, bucket={settings.AWS_STORAGE_WEB_BUCKET_NAME}")
             s3_obj = s3_client.get_object(
@@ -19,7 +25,7 @@ class PolaWebView(View):
             )
             body = s3_obj['Body'].read()
             content_type = s3_obj.get('ContentType') or 'application/octet-stream'
-            return HttpResponse(body, content_type=content_type, status=status_code)
+            return body, content_type
         except ClientError as ex:
             if ex.response['Error']['Code'] == 'NoSuchKey':
                 return None
@@ -31,21 +37,27 @@ class PolaWebView(View):
             return defaults.page_not_found(request, self.kwargs.get('exception', None))
         s3_client = create_s3_client()
         file_path = request.path.strip("/")
-        if file_path:
-            response = self.get_s3_stream_response(s3_client, file_path)
-            if response:
-                return response
-
-        splited_path = os.path.splitext(file_path)
-        if not splited_path[1]:
-            response = self.get_s3_stream_response(s3_client, os.path.join(file_path, "index.html"))
-            if response:
-                return response
-        response = self.get_s3_stream_response(s3_client, '404.html', 404)
-        if response:
+        for candidate_key, status_code in self.get_candidates(file_path):
+            s3_response = self.get_s3_data(s3_client, candidate_key)
+            if not s3_response:
+                continue
+            body, content_type = s3_response
+            response = HttpResponse(body, content_type=content_type, status=status_code)
+            if len(body) > MAX_CACHE_KEY_SIZE:
+                add_never_cache_headers(response)
             return response
 
         return defaults.page_not_found(request, self.kwargs.get('exception', None))
 
+    def get_candidates(self, file_path):
+        candidates = []
+        if file_path:
+            candidates.append((file_path, 200))
+        splited_path = os.path.splitext(file_path)
+        if not splited_path[1]:
+            candidates.append((os.path.join(file_path, "index.html"), 200))
+        candidates.append(('404.html', 404))
+        return candidates
 
-page_not_found_handler = cache_page(60 * 15)(PolaWebView.as_view())
+
+page_not_found_handler = cache_page(CACHE_TIMEOUT)(PolaWebView.as_view())
