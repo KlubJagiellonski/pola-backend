2a9e3334d8c3160ceeb4ea88508edbf1f4acafae
Kamil Breguła <kamilbregula@apache.org>
2021-10-18 16:18:38 +0200

    fixup! fixup! fixup! Add pola score field to BI export views

--- pola/bi_export/tests/test_views.py
+++ pola/bi_export/tests/test_views.py
@@ -18,45 +18,33 @@ class TestHome(PermissionMixin, TestCase):
         c2 = CompanyFactory(
             plCapital=100, plWorkers=100, plRnD=100, plRegistered=100, plNotGlobEnt=100, query_count=100
         )
-        ProductFactory(company=c1, query_count=33)
+        ProductFactory(company=c1, brand__company=c1, query_count=33)
 
         self.login()
         resp = self.client.get(self.url)
         csv_content = list(csv.DictReader(io.StringIO(resp.content.decode())))
-        from pprint import pprint
 
-        pprint(csv_content)
         self.assertEqual(
             [
                 {
                     'common_name': c2.common_name,
-                    'official_name': c2.official_name,
-                    'name': 'company1',
+                    'count_product_id': '0',
+                    'name': c2.name,
                     'nip': '',
+                    'official_name': c2.common_name,
+                    'pola_score': '100.00',
                     'query_count': '100',
-                    'count_product_id': '0',
                     'sum_product_product_query_count': '',
-                    'pola_score': '100.00',
                 },
                 {
-                    'common_name': 'company_official_0',
-                    'official_name': 'company_official_0',
-                    'name': 'company0',
-                    'nip': '',
-                    'query_count': '50',
+                    'common_name': c1.common_name,
                     'count_product_id': '1',
-                    'sum_product_product_query_count': '33',
-                    'pola_score': '100.00',
-                },
-                {
-                    'common_name': 'company_official_2',
-                    'official_name': 'company_official_2',
-                    'name': 'company2',
+                    'name': c1.name,
                     'nip': '',
-                    'query_count': '0',
-                    'count_product_id': '0',
-                    'sum_product_product_query_count': '',
-                    'pola_score': '-1',
+                    'official_name': c1.common_name,
+                    'pola_score': '100.00',
+                    'query_count': '50',
+                    'sum_product_product_query_count': '33',
                 },
             ],
             csv_content,
