41335b4c1bc1ee212f7c7bbd156f47655ca3c0fb
Kamil Breguła <kamilbregula@apache.org>
2021-10-18 15:21:23 +0200

    fixup! fixup! Add pola score field to BI export views

--- pola/bi_export/tests/test_views.py
+++ pola/bi_export/tests/test_views.py
@@ -15,17 +15,22 @@ class TestHome(PermissionMixin, TestCase):
 
     def test_generate_report(self):
         c1 = CompanyFactory(plCapital=100, plWorkers=100, plRnD=100, plRegistered=100, plNotGlobEnt=100, query_count=50)
-        CompanyFactory(plCapital=100, plWorkers=100, plRnD=100, plRegistered=100, plNotGlobEnt=100, query_count=100)
+        c2 = CompanyFactory(
+            plCapital=100, plWorkers=100, plRnD=100, plRegistered=100, plNotGlobEnt=100, query_count=100
+        )
         ProductFactory(company=c1, query_count=33)
 
         self.login()
         resp = self.client.get(self.url)
         csv_content = list(csv.DictReader(io.StringIO(resp.content.decode())))
+        from pprint import pprint
+
+        pprint(csv_content)
         self.assertEqual(
             [
                 {
-                    'common_name': 'company_official_1',
-                    'official_name': 'company_official_1',
+                    'common_name': c2.common_name,
+                    'official_name': c2.official_name,
                     'name': 'company1',
                     'nip': '',
                     'query_count': '100',
