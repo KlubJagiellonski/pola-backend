0ad5054c4f65a9c185bbfd2fcc7ede1afc4ece4b
Kamil Breguła <mik-laj@users.noreply.github.com>
2021-11-03 20:38:01 +0100

    Fix typo

--- pola/rpc_api/tests/test_views_v4.py
+++ pola/rpc_api/tests/test_views_v4.py
@@ -148,7 +148,7 @@ class TestGetByCodeV4(TestCase, JsonRequestMixin):
 
         self.assertEqual(200, response.status_code, response.content)
         self.assertEqual(
-            f"TEST\nMarki naależace do firmy: {brands[0]}, {brands[1]}, {brands[2]}, {brands[3]}, {brands[4]}.",
+            f"TEST\nMarki należace do firmy: {brands[0]}, {brands[1]}, {brands[2]}, {brands[3]}, {brands[4]}.",
             json.loads(response.content)['companies'][0]['description'],
         )
 
