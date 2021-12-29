5abf74cccad7de82753a6982af89268ce3bb5e1b
Kamil Breguła <kamilbregula@apache.org>
2021-12-29 19:40:34 +0100

    fixup! fixup! fixup! Add created and modifitied fields

--- pola-bi/dbt/models/pola_app/bi_popular_not_verified_products.sql
+++ pola-bi/dbt/models/pola_app/bi_popular_not_verified_products.sql
@@ -1,6 +1,6 @@
 SELECT *
 FROM  {{ source('public', 'product_product') }}
-WHERE created_at >= NOW() - INTERVAL '1 month'
+WHERE created >= NOW() - INTERVAL '1 month'
   AND company_id IS null
   AND query_count > 0
 ORDER BY query_count DESC
