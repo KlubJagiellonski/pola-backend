19799c1446ae15618e7418ceb692264bac95d506
MatPerowicz <52166969+MatPerowicz@users.noreply.github.com>
2022-01-11 16:06:42 +0100

    Update views_v4.py

--- pola/rpc_api/views_v4.py
+++ pola/rpc_api/views_v4.py
@@ -56,8 +56,8 @@ def get_by_code_internal(request, ai_supported=False, multiple_company_supported
 
     result["donate"] = {
         "show_button": True,
-        "title": "Wyszukaj produkty po nazwie",
-        "url": "https://www.pola-app.pl/",
+        "title": "Potrzebujemy 1 zł",
+        "url": "https://klubjagiellonski.pl/zbiorka/wspieraj-aplikacje-pola/",
     }
     return result
 
