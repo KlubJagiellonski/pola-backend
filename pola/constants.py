5b98a6137096ceecb10cc913b029920bdbe8f1f2
Kamil Breguła <kamilbregula@apache.org>
2022-03-03 17:46:10 +0100

    fixup! Add warning about RU/BY products (#1659)

--- pola/constants.py
+++ pola/constants.py
@@ -1,2 +1,5 @@
-DONATE_URL = "https://klubjagiellonski.pl/zbiorka/wspieraj-aplikacje-pola/"
+DONATE_URL = (
+    "https://klubjagiellonski.pl/2022/03/03/"
+    "odinstaluj-kasperskyego-nie-graj-w-world-of-tanks-bojkot-rosyjskich-produktow-w-aplikacji-pola/"
+)
 DONATE_TEXT = "Lista rosyjskich firm"
