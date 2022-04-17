da4d0d74961861c152a5120d8b8563b6737ab66d
Kamil Breguła <kamilbregula@apache.org>
2022-04-17 21:51:51 +0200

    Search by company name

--- pola/rpc_api/tests/test_views_v4.py
+++ pola/rpc_api/tests/test_views_v4.py
@@ -322,6 +322,27 @@ class TestSearchV4(TestCase):
         )
         self.assertTrue(SearchQuery.objects.filter(text="ciastec", client=device_id).exists())
 
+    def test_should_return_results_by_company_name(self):
+        c1 = CompanyFactory(name="bat")
+        p1 = ProductFactory(company=c1)
+        response = self.client.get(f"{self.url}?query=bat", content_type="application/json")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(
+            {
+                'nextPageToken': None,
+                'products': [
+                    {
+                        'brand': {'name': p1.brand.name},
+                        'code': p1.code,
+                        'company': {'name': p1.company.name, 'score': None},
+                        'name': p1.name,
+                    }
+                ],
+                'totalItems': 1,
+            },
+            json.loads(response.content),
+        )
+
     def test_should_return_results_by_product_code(self):
         p1 = ProductFactory(name="test-product")
         response = self.client.get(f"{self.url}?query={p1.code}", content_type="application/json")
