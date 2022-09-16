a90d3d8818aa2618d09ec83e5129903e174b7c72
MatPerowicz <52166969+MatPerowicz@users.noreply.github.com>
2022-09-16 13:05:58 +0200

    Chwilowa zmiana przycisku zbiórki

--- pola/constants.py
+++ pola/constants.py
@@ -1,2 +1,2 @@
-DONATE_URL = "https://klubjagiellonski.pl/zbiorka/wspieraj-aplikacje-pola/"
-DONATE_TEXT = "Potrzebujemy 1 zł"
+DONATE_URL = "https://klubjagiellonski.pl/2022/09/14/polska-flaga-na-produkcie-nic-nie-znaczy-jak-zrobiono-biznes-z-patriotyzmu/"
+DONATE_TEXT = "Fałszywe oznaczenia produktów"
