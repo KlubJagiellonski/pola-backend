79288a842ac9faec27402531bf5bf953114aae29
Maksymilian Skica <27677566+skica-dev@users.noreply.github.com>
2022-09-20 23:41:08 +0200

    Provided proper black formatting for this change

--- pola/constants.py
+++ pola/constants.py
@@ -1,2 +1,4 @@
-DONATE_URL = "https://klubjagiellonski.pl/2022/09/14/polska-flaga-na-produkcie-nic-nie-znaczy-jak-zrobiono-biznes-z-patriotyzmu/"
+DONATE_URL = (
+    "https://klubjagiellonski.pl/2022/09/14/polska-flaga-na-produkcie-nic-nie-znaczy-jak-zrobiono-biznes-z-patriotyzmu/"
+)
 DONATE_TEXT = "Fałszywe oznaczenia produktów"
