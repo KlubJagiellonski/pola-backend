c3f1a462ad181a88777cae642eca6d6e3a624c6d
Kamil Breguła <kamilbregula@apache.org>
2022-11-27 01:50:52 +0100

    fixup! fixup! Rewrite build script to Python

--- scripts/ci_docker_image.py
+++ scripts/ci_docker_image.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
+import difflib
+import itertools
 import os
 import shlex
 import subprocess
+import sys
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
@@ -70,6 +73,75 @@ def cmd_build_image(args):
     subprocess.run(['docker', 'tag', f"{CI_IMAGE_NAME}:{IMAGE_TAG}", "pola-backend_web:latest"], check=True)
 
 
+def side_by_side_diff(a, b):
+    cruncher = difflib.SequenceMatcher(a=a, b=b)
+    a_max_length = max(len(item) for item in a)
+    b_max_length = max(len(item) for item in b)
+
+    for tag, alo, ahi, blo, bhi in cruncher.get_opcodes():
+        if tag == 'replace':
+            for left, right in itertools.zip_longest(a[alo:ahi], b[blo:bhi], fillvalue=''):
+                print(left, end="")
+                print(" " * (a_max_length - len(left)), end="")
+                print("| ")
+                print(right, end="")
+                print(" " * (b_max_length - len(right)), end="")
+                print()
+        elif tag == 'delete':
+            for left in a[alo:ahi]:
+                print(left, end="")
+                print(" " * (a_max_length - len(left)), end="")
+                print("< ", end="")
+                print()
+        elif tag == 'insert':
+            for right in b[blo:bhi]:
+                print(" " * a_max_length, end="")
+                print("> ", end="")
+                print(right, end="")
+                print()
+        elif tag == 'equal':
+            for left_right in a[alo:ahi]:
+                print(left_right, end="")
+                print(" " * (a_max_length - len(left_right)), end="")
+                print("  ", end="")
+                print(left_right, end="")
+                print(" " * (b_max_length - len(left_right)), end="")
+                print()
+        else:
+            raise ValueError(f'unknown tag {tag!r}')
+
+
+def cmd_verify_image(args):
+    current_requirements = (
+        subprocess.check_output(
+            ['docker', 'run', '--entrypoint', '/bin/bash', '--rm', f'{CI_IMAGE_NAME}:{IMAGE_TAG}', '-c', "pip freeze"]
+        )
+        .decode()
+        .splitlines()
+    )
+    current_requirements = (d for d in current_requirements if d.strip())
+    current_requirements = [d for d in current_requirements if not d.lower().startswith('"Django==')]
+    current_requirements = sorted(current_requirements, key=lambda d: d.lower())
+
+    expected_requirements = (ROOT_DIR / "dependencies" / "constraints-ci.txt").read_text().splitlines()
+    expected_requirements = (d for d in expected_requirements if d.strip())
+    expected_requirements = [d for d in expected_requirements if not d.lower().startswith('"Django==')]
+    expected_requirements = sorted(expected_requirements, key=lambda d: d.lower())
+
+    if current_requirements != expected_requirements:
+        side_by_side_diff(current_requirements, expected_requirements)
+        sys.exit(1)
+    else:
+        print("Dependencies identical")
+
+
+def cmd_push_image(args):
+    image_name = f"{CI_IMAGE_NAME}:{IMAGE_TAG}"
+    print(f"Pushing image: {image_name!r}")
+    subprocess.run(['docker', 'tag', 'pola-backend_web', image_name], check=True)
+    subprocess.run(['docker', 'push', image_name], check=True)
+
+
 def get_parser():
     parser = argparse.ArgumentParser(prog=Path(__file__).name)
     subparsers = parser.add_subparsers(dest="subcommand", metavar="COMMAND")
@@ -77,6 +149,13 @@ def get_parser():
 
     build_image = subparsers.add_parser('build_image', help="Build image")
     build_image.set_defaults(func=cmd_build_image)
+
+    verify_image = subparsers.add_parser('verify_image', help="Verify image")
+    verify_image.set_defaults(func=cmd_verify_image)
+
+    push_image = subparsers.add_parser('push_image', help="Push image")
+    push_image.set_defaults(func=cmd_push_image)
+
     return parser
 
 
