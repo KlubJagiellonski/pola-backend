c7843c5d436bd6c355531f5b180b1b27d9b9ae88
MatPerowicz <52166969+MatPerowicz@users.noreply.github.com>
2023-02-27 14:20:03 +0100

    Update constants.py

--- pola/constants.py
+++ pola/constants.py
@@ -1,2 +1,2 @@
-DONATE_URL = "https://klubjagiellonski.pl/zbiorka/wspieraj-aplikacje-pola/"
-DONATE_TEXT = "Wspieraj aplikację Pola!"
+DONATE_URL = "https://klubjagiellonski.pl/petycje/auchan-zrozum-ze-nie-zawsze-sie-oplaca-petycja/"
+DONATE_TEXT = "Petycja do sieci Auchan!"
