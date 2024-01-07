6f6aa47b7490a3539232ff994e9fc9c68bc22b51
Kamil Breguła <kamilbregula@apache.org>
2024-01-07 09:14:16 +0100

    Fix comapny and brand image URLs

--- pola/rpc_api/tests/test_views.py
+++ pola/rpc_api/tests/test_views.py
@@ -1,4 +1,3 @@
-import io
 import json
 
 
@@ -6,16 +5,3 @@ class JsonRequestMixin:
     def json_request(self, url, data=None, **kwargs):
         body = json.dumps(data)
         return self.client.post(url, body, content_type="application/json", **kwargs)
-
-
-def _create_image(width=100, height=None, color='blue', image_format='JPEG', image_palette='RGB'):
-    # ImageField (both django's and factory_boy's) require PIL.
-    # Try to import it along one of its known installation paths.
-    from PIL import Image
-
-    height = height or width
-
-    thumb_io = io.BytesIO()
-    with Image.new(image_palette, (width, height), color) as thumb:
-        thumb.save(thumb_io, format=image_format)
-    return thumb_io.getvalue()
