e7559febb87c42ab3d02ece4e03ce7d4fec63bf1
MatPerowicz <52166969+MatPerowicz@users.noreply.github.com>
2024-04-18 20:42:37 +0200

    Update constants.py

--- pola/constants.py
+++ pola/constants.py
@@ -1,2 +1,2 @@
-DONATE_URL = "https://klubjagiellonski.pl/zbiorka/wspieraj-aplikacje-pola/"
-DONATE_TEXT = "Twój 1 zł może nam pomóc!"
+DONATE_URL = "https://www.pola-app.pl/1-5-podatku-na-aplikacje-pola"
+DONATE_TEXT = "1,5% podatku na aplikację Pola?"
