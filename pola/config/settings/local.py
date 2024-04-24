65b783246e5320028f23f52213d0181a8edc56a4
Kamil Breguła <kamilbregula@apache.org>
2024-04-24 15:29:11 +0200

    Wyłącz walidacje server dla OpenAPI-Core

--- pola/config/settings/local.py
+++ pola/config/settings/local.py
@@ -19,23 +19,21 @@ IS_DOCKER = Path('/.dockerenv').exists()
 DEBUG = env.bool('DJANGO_DEBUG', default=True)  # noqa: F405
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG  # noqa: F405
 
-# if IS_DOCKER:
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'web', '127.0.0.1']
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
-MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)  # noqa: F405
+MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware', 'pola.middlewares.SetHostToLocalhost')  # noqa: F405
 INSTALLED_APPS += ('debug_toolbar',)  # noqa: F405
 
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', '192.168.99.1', '192.168.0.1', '0.0.0.0']
-# if IS_DOCKER:
-# INTERNAL_IPS.append('0.0.0.0')
 
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': ['debug_toolbar.panels.redirects.RedirectsPanel'],
     'SHOW_TEMPLATE_CONTEXT': True,
     # 'SHOW_TOOLBAR_CALLBACK': lambda request: True
 }
+
 
 # django-extensions
 # ------------------------------------------------------------------------------
