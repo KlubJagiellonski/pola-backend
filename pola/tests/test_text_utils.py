171cccb98b946eea1c596d46faf1916929f44964
dokato <raymon92@gmail.com>
2025-09-29 16:04:49 +0100

    test_text_utils style fixes

--- pola/tests/test_text_utils.py
+++ pola/tests/test_text_utils.py
@@ -2,6 +2,7 @@ import pytest
 
 from pola.text_utils import _shorten_txt, strip_dbl_spaces, strip_urls_newlines
 
+
 def test_strip_dbl_spaces_basic():
     assert strip_dbl_spaces("  Ala   ma   kota  ") == "Ala ma kota"
 
